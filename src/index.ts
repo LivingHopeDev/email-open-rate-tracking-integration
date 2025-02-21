e2694d5e97ab64996044b5fd4f2e8284d6567847
adetayo adewobi <adewobiadetayo1@gmail.com>
2025-02-21 03:52:26 +0100

    feat: add integration.json file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import rootRouter from "./routes/index";
 import connectDB from "./config/db";
 import config from "./config";
 import cors from "cors";
+import path from "path";
+import fs from "fs";
 import { errorHandler, routeNotFound } from "./middlewares/error";
 const app = express();
 app.use(express.json());
 app.use(cors());
 // connectDB();
 // Routes
+app.get("/integration.json", (req: Request, res: Response) => {
+  const integration = fs.readFileSync(
+    path.join(__dirname, "integration.json"),
+    "utf-8"
+  );
+
+  res.status(200).json(JSON.parse(integration));
+});
 app.use("/api/v1", rootRouter);
 
 app.use(routeNotFound);
